ef5e0556e8fbc5aa0498f66d20630d1be24d456f
yukariko <rkdals7979@gmail.com>
2021-09-17 22:02:54 +0900

    fish: fix check omf command

--- init.d/002-install-fish.sh
+++ init.d/002-install-fish.sh
@@ -4,20 +4,16 @@ set -euo pipefail
 
 case "$1" in
     init)
-        command -v fish &> /dev/null && {
-            echo "Fish is already installed. Skip."
-            exit 0
-        }
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
             eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
         fi
 
         brew install fish
         # install omf
-        curl -L https://get.oh-my.fish | fish
+        fish -c omf &> /dev/null || curl -L https://get.oh-my.fish | fish
         ;;
     clean)
+        fish -c omf &> /dev/null && fish -c omf -- destroy
         command -v fish &> /dev/null && brew uninstall fish
-        command -v omf &> /dev/null && omf destroy
         ;;
 esac
