0b55e36d7ae750671c7a0ba75f069d05f7f50fea
yukariko <rkdals7979@gmail.com>
2021-09-17 22:20:13 +0900

    create install build tools script

---
+++ init.d/003-install-build-tools.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+case "$1" in
+    init)
+        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        fi
+        brew install gcc
+        gccpath=$(command -v gcc-11)
+        gpppath=$(command -v g++-11)
+        ln -s ./gcc-11 ${gccpath%-11}
+        ln -s ./g++-11 ${gpppath%-11}
+        ;;
+    clean)
+        rm -rf $(command -v gcc)
+        rm -rf $(command -v g++)
+        brew uninstall gcc
+        ;;
+esac
