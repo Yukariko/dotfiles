99e4ddb89faa6ac3fbc6d18f733ecabbdfcbe6e7
yukariko <rkdals7979@gmail.com>
2021-09-17 20:26:38 +0900

    remove '$?' check, seperate preinit script

--- init.sh
+++ init.sh
@@ -3,12 +3,21 @@
 RED='\033[0;31m'
 LGREEN='\033[1;32m'
 NC='\033[0m'
-for file in ./init.d/*; do
+for file in ./init.d/0*; do
     echo -e "run ${LGREEN}$(basename "$file")${NC}"
-    $file "init"
-    if [ $? -ne 0 ]; then
-        echo -e "${RED}failed.${NC}"
-    else
+    if $file "init"; then
         echo -e "${LGREEN}done.${NC}"
+    else
+        echo -e "${RED}failed.${NC}"
+        exit 1
+    fi
+done
+
+for file in ./init.d/[1-9]*; do
+    echo -e "run ${LGREEN}$(basename "$file")${NC}"
+    if $file "init"; then
+        echo -e "${LGREEN}done.${NC}"
+    else
+        echo -e "${RED}failed.${NC}"
     fi
 done
