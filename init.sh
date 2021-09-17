d094c78380ecb82c858b4d05a863c187d8b243cf
yukariko <rkdals7979@gmail.com>
2021-09-17 21:04:54 +0900

    source brew env for dependency

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@
 RED='\033[0;31m'
 LGREEN='\033[1;32m'
 NC='\033[0m'
+
+# run preinit scripts
 for file in ./init.d/0*; do
     echo -e "run ${LGREEN}$(basename "$file")${NC}"
     if $file "init"; then
@@ -13,6 +15,12 @@ for file in ./init.d/0*; do
     fi
 done
 
+# source brew environment after preinit
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+# run init scripts
 for file in ./init.d/[1-9]*; do
     echo -e "run ${LGREEN}$(basename "$file")${NC}"
     if $file "init"; then
