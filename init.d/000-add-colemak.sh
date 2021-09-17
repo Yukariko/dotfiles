b3dcca54e390630e6354221034f4b5c4080bb20b
yukariko <rkdals7979@gmail.com>
2021-09-17 20:25:53 +0900

    trace error with 'set -euopipefail'

--- init.d/000-add-colemak.sh
+++ init.d/000-add-colemak.sh
@@ -7,17 +7,20 @@ tmpPath="/tmp/colemak"
     exit 0
 }
 
+set -euo pipefail
+
 case "$1" in
     init)
-        git clone https://github.com/ColemakMods/mod-dh.git "$tmpPath" || exit 1
-        cp -r "$tmpPath/macOS/Colemak DH.bundle" ~/Library/Keyboard\ Layouts/ || {
+        git clone https://github.com/ColemakMods/mod-dh.git "$tmpPath"
+        cp -R "$tmpPath/macOS/Colemak DH.bundle" ~/Library/Keyboard\ Layouts/ || {
             rm -rf "$tmpPath"
             exit 1
         }
-        rm -rf "$tmpPath" || exit 1
+        rm -rf "$tmpPath"
         read -p "You need to update your keyboard layout in settings. [Enter]" yn
         ;;
     clean)
         read -p "You need to remove your colemak keyboard layout in settings. [Enter]" yn
-        rm -rf ~/Library/Keyboard\ Layouts/Colemak\ DH.bundle || exit 1
+        rm -rf ~/Library/Keyboard\ Layouts/Colemak\ DH.bundle
         ;;
+esac
