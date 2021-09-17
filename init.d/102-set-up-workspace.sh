40d1bbe46bc7f18f173f0bde402786dc1cde71f3
yukariko <rkdals7979@gmail.com>
2021-09-18 02:22:29 +0900

    workspace: git pull if repo is already cloned

--- init.d/102-set-up-workspace.sh
+++ init.d/102-set-up-workspace.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 case "$1" in
     init)
         mkdir ~/git || true
-        git clone git@github.com:Yukariko/ps_env.git ~/git/code
+        git clone git@github.com:Yukariko/ps_env.git ~/git/code || {
+            cd ~/git/code
+            git pull
+        }
         ;;
     clean)
         rm -rf ~/git
