b23b82e5e398fa5bcac9404f254d2b48090a4ed2
yukariko <rkdals7979@gmail.com>
2021-09-18 21:31:19 +0900

    fish: apply config files

--- init.d/002-install-fish.sh
+++ init.d/002-install-fish.sh
@@ -9,12 +9,22 @@ case "$1" in
         fi
 
         brew install fish
+
+        confpath="$(git rev-parse --show-toplevel)/conf.d"
+        if [ -e "$confpath/prompt.fish" ]; then
+            ln -s "$confpath/prompt.fish" ~/.config/fish/conf.d/prompt.fish || true
+        fi
+
+        if [ -e "$confpath/alias.fish" ]; then
+            ln -s "$confpath/alias.fish" ~/.config/fish/conf.d/alias.fish || true
+        fi
+
         # install omf
-	if [ -d "$HOME/.local/share/omf" ]; then 
-	    echo "Oh-my-fish is already installed. Skip."
-        exit 0
-	fi
-	fish -c omf &> /dev/null || curl -L https://get.oh-my.fish | fish
+        if [ -d "$HOME/.local/share/omf" ]; then
+            echo "Oh-my-fish is already installed. Skip."
+            exit 0
+        fi
+        fish -c omf &> /dev/null || curl -L https://get.oh-my.fish | fish
         ;;
     clean)
         fish -c omf &> /dev/null && fish -c omf -- destroy
