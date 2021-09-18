f11cc8f494d017287c9b8ac5dbeba13f8f056d0b
yukariko <rkdals7979@gmail.com>
2021-09-18 22:35:01 +0900

    misc: add ripgrep, add fzf

--- init.d/105-install-misc.sh
+++ init.d/105-install-misc.sh
@@ -7,10 +7,14 @@ case "$1" in
         else
             brew install ClementTsang/bottom/bottom
         fi
+        brew install ripgrep
+        brew install fzf
         brew install lsd
         ;;
     clean)
         brew uninstall bottom
         brew uninstall lsd
+        brew uninstall ripgrep
+        brew uninstall fzf
         ;;
 esac
