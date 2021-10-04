a01a5a67c9e9fd0afa87c31b8f34a231e7e9cc9a
yukariko <yukariko@github.com>
2021-10-05 03:46:59 +0900

    fish: reorder aliases, add bat alias

--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -2,6 +2,23 @@ function agi
     apt-get install $argv
 end
 
+function apt
+    sudo apt $argv
+end
+
+function akill
+    sudo kill -9 (ps -ef | grep "$argv" | awk "{print \$2}")
+end
+
+function btm
+    set path (which btm)
+    $path --color=nord $argv
+end
+
+function cat
+    bat $argv
+end
+
 function l
     lsd -a $argv --icon=never
 end
@@ -14,23 +31,10 @@ function ps
     /bin/ps -ef $argv
 end
 
-function ncore
-    cat /proc/cpuinfo | grep -c "processor" $argv
-end
-
 function mmv
     find . -maxdepth 1 -name "*$argv[1]" -exec bash -c 'mv "$1" "${1%$2}$3"' _ "{}" $argv[1] $argv[2] \;
 end
 
-function apt
-    sudo apt $argv
-end
-
-function akill
-    sudo kill -9 (ps -ef | grep "$argv" | awk "{print \$2}")
-end
-
-function btm
-    set path (which btm)
-    $path --color=nord $argv
+function ncore
+    cat /proc/cpuinfo | grep -c "processor" $argv
 end
