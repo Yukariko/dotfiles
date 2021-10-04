fa5714a9eccfb304027bb4660ccc0b091c10f52e
유카리코 <yukariko@yukalikoui-iMac.local>
2021-10-04 23:07:49 +0900

    brew: add brew path on macos

--- conf.d/brew.fish
+++ conf.d/brew.fish
@@ -2,4 +2,6 @@ switch (uname)
     case Linux
         set prefix (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
         eval "$prefix"
+    case Darwin
+        set PATH $PATH /opt/homebrew/bin
 end
