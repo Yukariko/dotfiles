8ad4126d92f9ab18f99d60c3daeed9ceafd61a0e
yukariko <yukariko@github.com>
2024-02-20 20:42:43 +0900

    Install tmux plugin manager

--- init.d/100-install-tmux.sh
+++ init.d/100-install-tmux.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 case "$1" in
     init)
         brew install tmux
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         confpath="$(git rev-parse --show-toplevel)/conf.d"
         ln -sF "$confpath/.tmux.conf" ~/.tmux.conf || true
         ;;
